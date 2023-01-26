f502225d0decee510b3663ba138c0e21d85ddd31
Keenan Ng <keenanng.dev@gmail.com>
2023-01-26 15:39:56 -0800

    User.js

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+//instantiate a new schema class
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+    maxLength: 20
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    enum: [
+      'Male',
+      'Female'
+    ]
+  },
+  email: {
+    type: String,
+    require: true,
+    unqiue: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+}, {
+  timeStamps: true
+})
+
+module.exports = mongoose.model('User')
