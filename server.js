2f50295ea353ab5768493dc31a4cd750088b58f5
Keenan Ng <keenanng.dev@gmail.com>
2023-01-26 14:21:42 -0800

    server.js filled out

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv')
+
+const app = express();
+
+const PORT = process.env.PORT || 5001
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`)
+})
+
+process.on('unhandledRejection', (err) => {
+	console.log(`Error: ${err.message}`)
+  server.close(() => process.exit(1))
+}
