31aaa1777c2638d1d6d944e7c16063faa52050c4
Keenan Ng <keenanng.dev@gmail.com>
2023-01-26 16:03:17 -0800

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 //import the mongodb connection module
 const connectDB = require('./config/db')
 const logger = require ('./utils/logger')
+// create a path to for the controller to listen to the user (more or less)
+const user = require('./routes/user')
 
 
 //configure the path for dotenv so that application can read from it
@@ -17,6 +19,7 @@ const PORT = process.env.PORT || 5001
 
 //use the logger after the port is defined
 app.use(logger)
+app.use('/user', user)
 
 const server = app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`)
