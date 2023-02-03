6f9738f9e3ff1a91c4e8915903a7cdc527122285
Keenan Ng <keenanng.dev@gmail.com>
2023-02-02 21:10:20 -0800

    codetrace

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,7 @@
 const logger = (req, res, next) => {
   console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`)
   //if you don't have next() it gets hung up and doenst know where to go from there.
+  // definitely look into this and code trace it to understand why
   next()
 }
 
